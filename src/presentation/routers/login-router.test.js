f5e0b47800533eab1c12f7129d76070352f351c4
Leonardo Sousa <leonardolsousa5@gmail.com>
2021-03-13 18:36:40 -0300

    teste unitario de rota de login com erro

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,6 +1,12 @@
+class LoginRouter {
+  route () {
+
+  }
+}
+
 describe('Login Router', () => {
   test('Should return 400 if no email is provided', () => {
-    const sut = new loginRouter() // System Under Test(sut) refere-se a um nome de variável padrão para teste unitário
+    const sut = new LoginRouter() // System Under Test(sut) refere-se a um nome de variável padrão para teste unitário
     const httpRequest = {
       body: {
         password: 'any_password'
