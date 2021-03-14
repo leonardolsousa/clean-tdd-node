1623733bf4168ba9c96b0aaa619f0aa3a87d2199
Leonardo Sousa <leonardolsousa5@gmail.com>
2021-03-14 17:27:12 -0300

    teste unitario de rota de login com sucesso de request

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,5 +1,10 @@
 class LoginRouter {
   route (httpRequest) {
+    if (!httpRequest) {
+      return {
+        statusCode: 500
+      }
+    }
     const { email, password } = httpRequest.body
     if (!email || !password) {
       return {
